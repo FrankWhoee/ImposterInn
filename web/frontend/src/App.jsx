72138c2c687c1c6d79a35d5e058fa845041327a5
FrankWhoee <huifrank@live.com>
2025-02-23 12:23:47 -0800

    checkpoint

--- web/frontend/src/App.jsx
+++ web/frontend/src/App.jsx
@@ -9,18 +9,25 @@ import Button from '@mui/material/Button';
 function App() {
   const [count, setCount] = useState(0)
 
+  var url = "ws://" + window.location.host + "/ws";
+  var ws = new WebSocket(url);
+  var mypid = localStorage.getItem("pid");
+  var mywid = "";
+
+  var renderQueue = [];
+
   return (
     <>
-      <h1>Imposter Inn</h1>
+      <h1>Liar's Fortress</h1>
 
-      <Stack spacing={2}>
-      <TextField id="outlined-basic" label="Lobby ID" variant="outlined" />
-      <Stack spacing={2} direction="row">
-        <Button variant="contained">Join Lobby</Button>
-        <Button variant="contained">Create Lobby</Button>
+      <Stack spacing={2} alignItems="center">
+        <TextField id="outlined-basic" label="Lobby ID" variant="outlined"/>
+        <Stack spacing={2} direction="row" alignItems="justify">
+          <Button variant="contained">Join Lobby</Button>
+          <Button variant="contained">Create Lobby</Button>
+        </Stack>
       </Stack>
-      </Stack>
-      
+
     </>
   )
 }
